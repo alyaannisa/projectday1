f05a8e2736e079f3415a298aa24b5a1be299aaaa
alyaannisa <rimdanialya10@gmail.com>
2024-02-07 07:59:30 +0700

    assigment pengelolaan kontak

--- php/hacktiv8/assigment-day7/edit.php
+++ php/hacktiv8/assigment-day7/edit.php
@@ -7,27 +7,6 @@ if (empty($_GET['id'])) header("Location: index.php");
 
 $id = $_GET['id'];
 
-// if(isset($_POST['simpan'])){
-//     $nama = $_POST['nama'];
-//     $email = $_POST['email'];
-//     $notelp = $_POST['notelp'];
-
-//     $pdo = $koneksi->conn;
-//     $sql = "UPDATE kontak set nama = ?, email = ?, notelp = ? WHERE id = ?";
-//     $q = $pdo->prepare($sql);
-//     $q->execute(array($id, $nama, $email, $notelp));
-
-//     header("Location: index.php");
-// } else {
-//     $pdo = $koneksi->conn;
-//     $sql = "SELECT * FROM kontak WHERE id = ?";
-//     $q = $pdo->prepare($sql);
-//     $q->execute(array($id));
-//     $data = mysqli_fetch_all($q->get_result());
-//     $nama = $data[0][1];
-//     $email = $data[0][2];
-//     $notelp = $data[0][3];
-// }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = $_GET['id'];
     $nama = $_POST['nama'];
@@ -76,24 +55,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="container" style="text-align: center; padding-top: 20px; padding-bottom: 40px;">
                 <h3>Edit Data</h3>
             </div>
-            <!-- <form action="edit.php?id=<?php echo $id ?>" method="POST">
-                    <div>
-                        <label>Nama</label>
-                        <input name="nama" type="text" placeholder="Masukkan Nama" required value="<?php echo $nama; ?>">
-                    </div>
-                    <div>
-                        <label>Email</label>
-                        <input name="email" type="text" placeholder="Masukkan Email" required value="<?php echo $email; ?>">
-                    </div>
-                    <div>
-                        <label>No Telepon</label>
-                        <input name="notelp" type="number" placeholder="Masukkan Nama" required value="<?php echo $notelp; ?>">
-                    </div>
-                    <div>
-                        <button type="submit" name="simpan">Simpan</button>
-                        <a href="index.php">Kembali</a>
-                    </div>
-                </form> -->
             <div class="container">
                 <form action="edit.php?id=<?php echo $id ?>" method="POST">
                     <div class="mb-2">
